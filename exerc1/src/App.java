19d9bc911f71c95a47558ce6a0a4346ed9a0f7d9
RAPHAEL BEN HUR AMARAL MASSAPUST <rmassapust@hotmail.com>
2022-08-23 17:10:46 -0300

    funcionado

---
+++ exerc1/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+     Scanner enterScanner = new Scanner(System.in);
+     int a;
+     int b;
+     System.out.println("Digite o primeiro numero da soma");
+     a = enterScanner .nextInt();
+     System.out.println("Digite o segundo numero da soma:");
+     b = enterScanner .nextInt();
+     int x;
+     x = a + b;
+
+     System.out.println("O resultado da soma foi de: X =" + x );
+ }
+}
