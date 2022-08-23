af1fb81ea4ea30ec7f9c3ff1fe8a78a369e60a1b
RAPHAEL BEN HUR AMARAL MASSAPUST <rmassapust@hotmail.com>
2022-08-23 19:37:25 -0300

    atualizado

---
+++ exerc1/src/App2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App2 {
+    public static void main(String[] args) {
+        Scanner enterScanner = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("teste 1:");
+        a = enterScanner .nextInt();
+        System.out.println("tete 2:");
+        b = enterScanner .nextInt();
+        int x;
+        x = a + b;
+   
+        System.out.println("O resultado da soma foi de: X =" + x );
+    }
+   }
