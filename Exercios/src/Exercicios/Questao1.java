76e6b3259890c6956c2dc09e687c3c7d159b469d
RAPHAEL BEN HUR AMARAL MASSAPUST <rmassapust@hotmail.com>
2022-08-30 00:26:46 -0300

    renomeado pelo nome questão

--- Exercios/src/Exercicios/Questao1.java
+++ Exercios/src/Exercicios/Questao1.java
@@ -1,7 +1,7 @@
 package Exercicios;
 import java.util.Scanner;
 
-public class App1 {
+public class Questao1 {
     public static void main(String[] args) {
      Scanner enterScanner = new Scanner(System.in);
      int a;
