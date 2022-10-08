243547665e0c31714e65a76a412728510c6a18d4
RAPHAEL BEN HUR AMARAL MASSAPUST <91230064+massapust@users.noreply.github.com>
2022-10-08 15:11:54 -0300

    revisão
    atualizando lista de exercicios

---
+++ Exercios/src/pratica_20220915/exerc1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Leia 3 valores reais (A, B e C) e verifique se eles formam ou
+ * não um triângulo. Em caso positivo,
+ * calcule o perímetro do triângulo e apresente a mensagem:
+ * Perimetro = XX.X
+ * Em caso negativo, calcule a área do trapézio
+ * que tem A e B como base e C como altura, mostrando a mensagem
+ * Area = XX.X
+ * Entrada
+ * A entrada contém três valores reais.
+ * Saída
+ * O resultado deve ser apresentado com uma casa decimal.
+ * <p>
+ * | b - c | < a < b + c
+ * | a - c | < b < a + c
+ * | a - b | < c < a + b
+ */
+
+public class exerc1 {
+
+    public static Boolean c1T(Double x, Double y, Double z) {
+        return Math.abs(x - y) < z;
+    }
+
+    public static Boolean c2T(Double x, Double y, Double z) {
+        return x < y + z;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        Double c = sc.nextDouble();
+        if (c1T(b, c, a) && c2T(a, b, c) &&
+                c1T(a, c, b) && c2T(b, a, c) &&
+                c1T(a, b, c) && c2T(c, a, b)) {
+            System.out.println(" perimetro =" + (a + b + c));
+        } else {
+            System.out.println("a area e = " + (((a + b) * c) / 2));
+        }
+    }
+}
---
+++ Exercios/src/pratica_20220915/exerc2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exerc2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salario = sc.nextDouble();
+        if (salario >= 0.0 && salario <= 2000.0){
+            System.out.print(0);
+        }else if (salario >= 2000.01  && salario<= 3000.0){
+           System.out.print(salario * 0.08);
+        }else if (salario >= 3000.01  && salario<= 4500.0){
+            System.out.print(salario * 0.18);
+        }else if (salario > 4500.0){
+            System.out.print(salario * 0.28);
+
+        }
+
+
+
+    }
+}
