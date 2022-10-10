3424653e64a2d9c98550f881bbbb51a36ac7b9c0
RAPHAEL BEN HUR AMARAL MASSAPUST <91230064+massapust@users.noreply.github.com>
2022-10-09 23:08:30 -0300

    verificando estruturas

--- Exercios/src/entrega_20220828/Questao3.java
+++ Exercios/src/entrega_20220828/Questao3.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class Questao3 {
     public static void main(String[] args) {
-     Scanner enterScanner = new Scanner(System.in);
-     int a;
-     int b;
-     System.out.println("Digite o primeiro numero da soma:");
-     a = enterScanner .nextInt();
-     System.out.println("Digite o segundo numero da soma:");
-     b = enterScanner .nextInt();
-     int s;
-     s = a + b;
+     try (Scanner enterScanner = new Scanner(System.in)) {
+        int a;
+         int b;
+         System.out.println("Digite o primeiro numero da soma:");
+         a = enterScanner .nextInt();
+         System.out.println("Digite o segundo numero da soma:");
+         b = enterScanner .nextInt();
+         int s;
+         s = a + b;
 
-     System.out.println("O resultado da soma foi de: SOMA = " + s );
+         System.out.println("O resultado da soma foi de: SOMA = " + s );
+    }
  }
 }
--- Exercios/src/entrega_20220828/Questão2.java
+++ Exercios/src/entrega_20220828/Questão2.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Questão2 {
     public static void main(String[] args) {
-        Scanner enterScanner = new Scanner(System.in);
-        double r, n, area;
-        n = 3.14159;
-        System.out.println("Informe o raio:");
-        r = enterScanner .nextInt();
-        area = n * r * r;
-   
+        try (Scanner enterScanner = new Scanner(System.in)) {
+            double r, n, area;
+            n = 3.14159;
+            System.out.println("Informe o raio:");
+            r = enterScanner .nextInt();
+            area = n * r * r;
+        }
         System.out.println("O valor da area é: A=" + area );
     }
    }
